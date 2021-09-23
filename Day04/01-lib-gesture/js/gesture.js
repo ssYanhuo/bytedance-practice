0ddbe59675de8e387b918c7b5c133f69b6de712a
三生烟火 <44744968+ssYanhuo@users.noreply.github.com>
2021-09-23 13:17:59 +0800

    added Day04\01-lib-gesture

--- Day04/01-lib-gesture/js/gesture.js
+++ Day04/01-lib-gesture/js/gesture.js
@@ -5,63 +5,120 @@
 -tap
 */
 
+
 function enableGesture(el){
     let contexts = {}
     const MOUSE_TYPE = Symbol('mouse')
     if (!('ontouchstart' in document)){
-        el.addEventListener('mousedown', () => {
+        el.addEventListener('mousedown', e => {
+            contexts[MOUSE_TYPE] = {}
+            onStart(e, contexts[MOUSE_TYPE])
             let move = e => {
-                console.log('move')
+                onMove(e, contexts[MOUSE_TYPE])
             }
             let end = e => {
-                console.log('end')
+                onEnd(e, contexts[MOUSE_TYPE])
+                document.removeEventListener('mousemove', move)
             }
-            console.log('start')
             document.removeEventListener('mousemove', move)
             document.removeEventListener('mouseup', end, { once: true })
         })
     }
     el.addEventListener('touchstart', e => {
         for(let touch of e.changedTouches){
-            onStart(touch)
+            contexts[touch.identifier] = {}
+            onStart(touch, contexts[touch.identifier])
         }
-        console.log('start')
     })
     el.addEventListener('touchmove', e => {
         for(let touch of e.changedTouches){
-            onMove(touch)
+            onMove(touch, contexts[touch.identifier])
         }
-        console.log('move')
     })
     el.addEventListener('touchend', e =>  {
         for(let touch of e.changedTouches){
-            onUp(touch)
+            onEnd(touch, contexts[touch.identifier])
+            delete contexts[touch.identifier]
         }
-        console.log('end')
     })
     let onStart = (e, context) =>{
         el.dispatchEvent(Object.assign(new CustomEvent('start'), {
             clientX: e.clientX,
             clientY: e.clientY
         }))
+        context.isTapping = true
         context.startX = e.clientX
         context.startY = e.clientY
+        clearTimeout(context.timeout)
+        context.timeout = setTimeout(() => {
+            context.isTapping = false
+            context.isPaning = true
+            el.dispatchEvent(Object.assign(new CustomEvent('pressstart'), {
+                clientX: e.clientX,
+                clientY: e.clientY
+            }))
+        }, 500)
     }
     let onMove = (e, context) =>{
         let dx = e.clientX - context.startX
         let dy = e.clientY - context.startY
-        if(dx ** 2 + dy ** 2 > 100 && !context.isPaning){
-            context.isPaning = true
-        }
-        if (context.isPaning){
-
-        }
-        el.dispatchEvent(Object.assign(new CustomEvent('move'), {
+        el.dispatchEvent((Object.assign(new CustomEvent('move'), {
             clientX: e.clientX,
             clientY: e.clientY
-        }))
+        })))
+        if(dx ** 2 + dy ** 2 > 100 && !context.isPaning){
+            context.isPaning = true
+            if(context.isPressing){
+                el.dispatchEvent(Object.assign(new CustomEvent('presscancel'), {
+                    clientX: e.clientX,
+                    clientY: e.clientY
+                }))
+            }
+            clearTimeout(context.timeout)
+            context.isTapping = false
+            context.isPressing = false
+            el.dispatchEvent(Object.assign(new CustomEvent('panstart'), {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                startX: e.startX,
+                startY: e.startY
+            }))
+            return
+        }
+        if (context.isPaning){
+            el.dispatchEvent(Object.assign(new CustomEvent('pan'), {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                startX: e.startX,
+                startY: e.startY
+            }))
+        }
     }
-    let onUp = e =>{
+    let onEnd = (e, context) =>{
+        clearTimeout(context.timeout)
+        if(context.isPaning){
+            el.dispatchEvent(Object.assign(new CustomEvent('penend'), {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                startX: context.startX,
+                startY: context.startY
+            }))
+            context.isPaning = false
+        }
+        if(context.isTapping){
+            el.dispatchEvent((Object.assign(new CustomEvent('tap'), {
+                clientX: e.clientX,
+                clientY: e.clientY
+            })))
+            context.isTapping = false
+        }
+        if(context.isPressing){
+            el.dispatchEvent(Object.assign(new CustomEvent('pressend'), {
+                clientX: e.clientX,
+                clientY: e.clientY
+            }))
+            context.isPressing = false
+        }
         el.dispatchEvent(Object.assign(new CustomEvent('end'), {
             clientX: e.clientX,
             clientY: e.clientY
